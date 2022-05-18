7092319dfad00f34635de68a4c0db564c99a79c1
hatz <hatz@localhost.localdomain>
2022-05-19 02:56:18 +0530

    fixed

--- pages/_document.js
+++ pages/_document.js
@@ -3,10 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head>
-        <script src="https://unpkg.com/pdf-lib@1.4.0"></script>
-        <script src="https://unpkg.com/@pdf-lib/fontkit@0.0.4"></script>
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
